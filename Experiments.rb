44f5326a0732c48ae06e2abfcbba87197dbbb3af
Simon Bushell <srbushell@gmail.com>
2012-11-15 14:44:04 +0000

    Created RubyPrimer and Experiment boilerplate, regex for experiment types

---
+++ Experiments.rb
@@ -0,0 +1,28 @@
+require "Snippet"
+
+class Experiment
+
+	def initialize
+	end
+end
+
+
+class DeletionExperiment < Experiment
+	
+	def initialize
+	end
+end
+
+
+class InsertionExperiment < Experiment
+	
+	def initialize
+	end
+end
+
+
+class SubstituionExperiment < Experiment
+	
+	def initialize
+	end
+end
