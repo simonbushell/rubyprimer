25e60522803e21f7c22062f80a879107520408bc
Simon Bushell <srbushell@gmail.com>
2012-11-27 11:08:03 +0000

    made getBestCodon method for experiment super class

--- Experiments.rb
+++ Experiments.rb
@@ -1,4 +1,6 @@
 require "./Snippet"
+require "Bio"
+require "amatch"
 
 class ExperimentError < StandardError ; end
 
@@ -29,6 +31,15 @@ class Experiment
 		end
 	end
 
+	def getBestCodon(codon, aminoacid)
+		include Amatch
+		codonTable = Bio::CodonTable[1]
+		possibleCodons = codonTable.revtrans(aminoacid)
+		codonScores = Hash.new
+		m = Sellers.new(codon)
+		possibleCodons.each {|x| codonScores[x] = m.match(x)}
+		return codonScores.sort_by{|codon, score| score}[0][0]
+	end
 end
 
 
--- scratchpad.rb
+++ scratchpad.rb
@@ -1,18 +1,13 @@
-class Foo
-	
-	attr_accessor :vari
+require "./Snippet"
+require "Bio"
+require "amatch"
 
-	def initialize(vari)
-		@vari = vari
-	end
-end
-
-
-class Bar
-
-	attr_accessor :vari
-
-	def initialize(vari)
-		@vari = vari
-	end
+def getBestCodon(codon, aminoacid)
+	include Amatch
+	codonTable = Bio::CodonTable[1]
+	possibleCodons = codonTable.revtrans(aminoacid)
+	codonScores = Hash.new
+	m = Sellers.new(codon)
+	possibleCodons.each {|x| codonScores[x] = m.match(x)}
+	return codonScores.sort_by{|codon, score| score}[0][0]
 end
